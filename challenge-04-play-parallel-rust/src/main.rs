681e042775e7d1d62da210428f7f9785de5e977d
Eric Silberstein <ericsilbersteinauthor@gmail.com>
2025-10-22 12:09:20 -0400

    started challenge 4 and added a first example of doing something in parallel

--- challenge-04-play-parallel-rust/src/main.rs
+++ challenge-04-play-parallel-rust/src/main.rs
@@ -1,3 +1,21 @@
+use std::time::Instant;
+use rayon::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    let numbers: Vec<u64> = (1..1_000_000_000).collect();
+
+    println!("--adding up {} numbers with regular iteration and fold--", numbers.len());
+    let start = Instant::now();
+    let sum = numbers.iter().fold(0, |acc, number| acc + number);
+    println!("sum: {}", sum);
+    println!("time elapsed: {:?}\n", start.elapsed());
+
+    println!("--adding up {} numbers with parallel iteration and fold--", numbers.len());
+    let start = Instant::now();
+    let interim: Vec<u64> = numbers.par_iter().fold(|| 0, |acc, number| acc + number).collect();
+    println!("interim.len(): {:?}", interim.len());
+    println!("interim.sum(): {}", interim.iter().sum::<u64>());
+    println!("time elapsed: {:?}\n", start.elapsed());
+    
 }
